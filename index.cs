608e66acf1e18bcde2cb0c1c75b48e6641fc7c78
Tiagotj7 <tiagowork07@gmail.com>
2024-03-21 11:29:45 -0300

    confing

---
+++ index.cs
@@ -0,0 +1,18 @@
+//projeto de conhecimento em C#!!!
+
+using System;
+
+class index {
+
+// Declaração e inicialização de variáveis
+    static void main(){
+
+        int x = 5;
+        int y = 7;
+        int z = 3;
+
+
+
+    }
+
+}
